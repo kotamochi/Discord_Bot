7ffcf616263ae5ef4efb6b82e2827638f9de22e1
kotamochi <kota.mochi.v2@gmail.com>
2023-09-19 20:28:08 +0900

    Arcaeaクラン用のbotを作成

--- ArcaeaClan/Arcaea_command.py
+++ ArcaeaClan/Arcaea_command.py
@@ -2,38 +2,8 @@ import pandas as pd
 import random
 import math
 
-def Random_Select_Const(const1="0", const2="12.0"):
-    
-    #定数を決めていない時は全曲から選ぶ
-    const1 = float(const1)
-    const2 = float(const2)
-    
-    #楽曲情報をデータフレームに読み込む
-    df_music = pd.read_csv("Arcaea_Music_Data.csv")
-    
-    #楽曲数を取得
-    df_music = df_music[df_music["FTR_Const"] >= const1]
-    df_music = df_music[df_music["FTR_Const"] <= const2]
-    
-    #乱数の範囲を取得
-    music_num = len(df_music)
 
-    rand = random.randint(0,music_num-1)
-
-    #乱数から選ばれた楽曲を抽出
-    hit_music = df_music.iloc[rand]
-
-    #結果を保存
-    music = hit_music["Music_Title"]
-    level = hit_music["FTR_Level"]
-    
-    if level % 1 != 0.0:
-        level_str = str(math.floor(level)) + "+"
-    else:
-        level_str = str(math.floor(level))
-        
-    return music, level_str
-
+#難易度ランダム選曲
 def Random_Select_Level(level1="0", level2="12"):
     
     #＋難易度が指定された時は.7表記に変更する
@@ -59,8 +29,107 @@ def Random_Select_Level(level1="0", level2="12"):
     df_music = pd.read_csv("Arcaea_Music_Data.csv")
     
     #楽曲数を取得
-    df_music = df_music[df_music["FTR_Level"] >= level1]
-    df_music = df_music[df_music["FTR_Level"] <= level2]
+    df_music_FTR = df_music[df_music["FTR_Level"] >= level1].copy()
+    df_music_FTR = df_music_FTR[df_music["FTR_Level"] <= level2]
+
+    df_music_BYD = df_music[df_music["BYD_Level"] >= level1].copy()
+    df_music_BYD = df_music_BYD[df_music["BYD_Level"] <= level2]
+
+    df_range_music = pd.concat([df_music_FTR, df_music_BYD])
+    
+    #乱数の範囲を取得
+    music_num = len(df_range_music)
+
+    #乱数を作成
+    rand = random.randint(0,music_num-1)
+
+    #乱数から選ばれた楽曲を抽出
+    hit_music = df_range_music.iloc[rand]
+
+    #結果を保存
+    music = hit_music["Music_Title"]
+    if pd.isnull(hit_music["BYD_Level"]) == True: #BYDのレベルデータがあるデータならBYDを結果として出力する
+        level = hit_music["FTR_Level"]
+        deffecult = "FTR" #難易度を表示
+    else:
+        level = hit_music["BYD_Level"]
+        deffecult = "BYD" #難易度を表示
+
+    #楽曲レベルを表示用に調整
+    if level % 1 != 0.0:
+        level_str = str(math.floor(level)) + "+"
+    else:
+        level_str = str(math.floor(level))
+        
+    return music, level_str, deffecult
+
+#ポテンシャル値の計算
+def Potential_Score(music, score, difficult="FTR"):
+
+    #楽曲情報をデータフレームに読み込む
+    df_music = pd.read_csv("Arcaea_Music_Data.csv")
+
+    #ptを算出したい曲のデータを取得
+    pt_music = df_music[df_music["Music_Title"] == music]
+
+    #登録されている略称(ニックネーム)でも可
+    if pt_music["Music_Title"].empty == True:
+        pt_music = df_music[df_music["Nickname"] == music]
+
+    #曲の譜面定数情報を取得
+    if difficult == "BYD" or difficult == "byd": #BYDの定数を取得
+        pt_music_b = pt_music.dropna(subset=["BYD_Const"])
+        const = float(pt_music_b["BYD_Const"].values)
+        difficult = "BYD"                        #返信用に形式を統一
+
+    else:                                        #FTRの定数を取得
+        pt_music_f = pt_music.dropna(subset=["FTR_Const"])
+        const = float(pt_music_f["FTR_Const"].values)
+
+    #スコアの桁数を合わせる(994と入力したものを9,940,000に直す)
+    while True:
+        score_digit = len(str(score)) #現在の桁数を取得
+        if score_digit == 7 or score_digit == 8:
+            break
+        score = score * 10
+
+    #スコア区分を変数として作成
+    PM = 10000000
+    EX = 9800000
+    AA = 9500000
+
+    #楽曲のポテンシャル値を計算
+    if score >= PM:
+        potential = const + 2                       #譜面定数+2.0
+
+    elif score >= EX:
+        potential = const + 1 + (score - EX)/200000 #譜面定数+1.0+(スコア-9,800,000)/200,000
+
+    else:
+        potential = const + (score - AA)/300000     #譜面定数+(スコア-9,500,000)/300,000 (下限は0)
+
+        #※ポテンシャル値は0以下にならない
+        if potential <= 0:
+            potential = 0
+
+    return round(potential, 2), score, difficult #少数第３位を四捨五入して表示
+
+
+
+#現在使用していない機能
+#定数ランダム選曲
+def Random_Select_Const(const1="0", const2="12.0"):
+    
+    #定数を決めていない時は全曲から選ぶ
+    const1 = float(const1)
+    const2 = float(const2)
+    
+    #楽曲情報をデータフレームに読み込む
+    df_music = pd.read_csv("Arcaea_Music_Data.csv")
+    
+    #楽曲数を取得
+    df_music = df_music[df_music["FTR_Const"] >= const1]
+    df_music = df_music[df_music["FTR_Const"] <= const2]
     
     #乱数の範囲を取得
     music_num = len(df_music)
--- ArcaeaClan/Arcaeabot.py
+++ ArcaeaClan/Arcaeabot.py
@@ -1,3 +1,4 @@
+import os
 import json
 import discord
 import Arcaea_command
@@ -18,30 +19,93 @@ async def on_ready():
     #起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
+#ID系統の設定
+MemberRole_ID = 1149393993159942214
+RandomSelect_CH = 1148058499520135198
+
+#サーバー入室時にロールを付与する
+@client.event
+async def on_member_join(member):
+    # 用意したIDから Role オブジェクトを取得
+    role = member.guild.get_role(MemberRole_ID)
+
+    # 入ってきた Member に役職を付与
+    await member.add_roles(role)
+
 #メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    #課題曲を作成する
-    if message.content.startswith('/a rand'):
-        #渡されたコマンドを分割
-        comannd = message.content.split(' ')
-        
-        #譜面定数上下限を設定してる時
-        if len(comannd) == 4:
-            level_low = comannd[2]
-            level_high = comannd[3]
-            music, level_str = Arcaea_command.Random_Select_Level(level_low, level_high)
-        
-        #譜面定数の下限を設定している時
-        elif len(comannd) == 3:
-            level = comannd[2]
-            music, level_str = Arcaea_command.Random_Select_Level(level)
-        
-        #譜面定数を設定していない時
-        else:
-            music, level_str = Arcaea_command.Random_Select_Level()
-        
-        #ランダムで決まった曲を返信
-        await message.channel.send(f"課題曲:{music} FTR:{level_str}です!!")
+    #ランダム選曲チャンネル以外では反応しない
+    if message.channel.id == RandomSelect_CH:
+        #課題曲を作成する
+        if message.content.startswith('/a rand'):
+            try:
+                #渡されたコマンドを分割
+                comannd = message.content.split(' ')
 
-client.run(TOKEN)
+                #譜面定数上下限を設定してる時
+                if len(comannd) == 4:
+                    level_low = comannd[2]
+                    level_high = comannd[3]
+                    music, level_str, dif  = Arcaea_command.Random_Select_Level(level_low, level_high)
+
+                #譜面定数の下限を設定している時
+                elif len(comannd) == 3:
+                    level = comannd[2]
+                    music, level_str, dif  = Arcaea_command.Random_Select_Level(level)
+
+                #譜面定数を設定していない時
+                elif message.content == "/a rand":
+                    music, level_str, dif = Arcaea_command.Random_Select_Level()
+
+                #ランダムで決まった曲を返信
+                await message.channel.send(f"課題曲:{music} {dif}:{level_str}です!!")
+            except Exception:
+                await message.channel.send("コマンドが間違ってるかも。もう一度試してみて!")
+
+        #1v1勝負をする
+        if message.content.startswith('/a vs'):
+            try:
+                #渡されたコマンドを分割
+                comannd = message.content.split(' ')
+                
+            
+            except Exception:
+                await message.channel.send("コマンドが間違ってるかも。もう一度試してみて!")
+
+
+
+        #ポテンシャルを計算する
+        #if message.content.startswith('/a pt'):
+        #    comannd = message.content.split(' ')
+        #    music_ls = []
+        #    try:
+        #        score = int(comannd[-1])
+        #        byd_flg = "FTR"
+        #        for i in range(len(comannd)-3):
+        #            music_ls.append(comannd[i+2])
+
+        #    except ValueError:
+        #        score = int(comannd[-2])
+        #        byd_flg = comannd[-1]
+
+        #        for i in range(len(comannd)-4):
+        #            music_ls.append(comannd[i+2])
+
+        #    #listから曲名を作成
+        #    music = " ".join(music_ls)
+
+        #    #ポテ計算を実行
+        #    potential, score_r, difficult = Arcaea_command.Potential_Score(music, score, byd_flg)
+
+        #    #返信文を作成
+        #    reply = f"Music:{music} ({difficult})\nScore:{score_r}\nPt:{potential}"
+
+        #    #メンションをつけて返信
+        #    await message.reply(reply)
+
+try:
+    client.run(TOKEN)
+except discord.errors.HTTPException:
+    os.system('kill 1')
+    os.system("python restarter.py")
